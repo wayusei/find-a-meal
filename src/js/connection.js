c60d59e0041516840d2d330b29a403c2d2df6054
angel-pm <angel.pime.99@gmail.com>
2022-08-29 12:46:44 -0500

    Se agregaron los metodos de busquedaTexto y busquedaRandom

--- src/js/connection.js
+++ src/js/connection.js
@@ -1,36 +1,19 @@
-/*
-
-getMealsName()
-.then(function(data) {
-    console.log(data);
-})
-
-
-function getMealsName() {
-    return fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-        .then(function (response) {
-            return response.json();
-        })
-}*/
-//document.getElementById("index-div").addEventListener("load", indexLoad);
-
 document.addEventListener('DOMContentLoaded', indexLoad, false);
 document.getElementById("btn_search").addEventListener("click", busquedaTexto);
 document.getElementById("btn_random").addEventListener("click", busquedaRandom);
 
-function indexLoad(){
+let btnRandom = document.getElementById("btn_random");
+btnRandom.addEventListener('click', busquedaRandom);
+
+function indexLoad() {
     console.log("Loaded")
 
-    const tresRecetas=document.getElementById("carrusel");   
-
-
-
-const texto="b";
-const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${texto}`;
+    const tresRecetas = document.getElementById("carrusel");
+    const texto = "b";
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${texto}`;
     fetch(url).then((res) => {
         if (res.status != "200") {
             console.log(res);
-            
         }
         else {
             return res.json();
@@ -40,98 +23,82 @@ const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${texto}`;
 
             console.log(data.meals);
             //data.meals.forEach(element => {
-            for(let i=0;i<3;i++){
-                const element=data.meals[i];
-                
-                const receta = {
-                    nombre:element.strMeal,
-                    ingredientes:[  element.strIngredient1,
-                                    element.strIngredient2,
-                                    element.strIngredient3,
-                                    element.strIngredient4,
-                                    element.strIngredient5,
-                                    element.strIngredient6,
-                                    element.strIngredient7,
-                                    element.strIngredient8,
-                                    element.strIngredient9,
-                                    element.strIngredient10,
-                                    element.strIngredient11,
-                                    element.strIngredient12,
-                                    element.strIngredient13,
-                                    element.strIngredient14,
-                                    element.strIngredient15,
-                                    element.strIngredient16,
-                                    element.strIngredient17,
-                                    element.strIngredient18,
-                                    element.strIngredient19,
-                                    element.strIngredient20],
+            for (let i = 0; i < 3; i++) {
+                const element = data.meals[i];
 
-                    cantidades:[    element.strMeasure1,
-                                    element.strMeasure2,
-                                    element.strMeasure3,
-                                    element.strMeasure4,
-                                    element.strMeasure5,
-                                    element.strMeasure6,
-                                    element.strMeasure7,
-                                    element.strMeasure8,
-                                    element.strMeasure9,
-                                    element.strMeasure10,
-                                    element.strMeasure11,
-                                    element.strMeasure12,
-                                    element.strMeasure13,
-                                    element.strMeasure14,
-                                    element.strMeasure15,
-                                    element.strMeasure16,
-                                    element.strMeasure17,
-                                    element.strMeasure18,
-                                    element.strMeasure19,
-                                    element.strMeasure20,],
+                const receta = {
+                    nombre: element.strMeal,
+                    ingredientes: [element.strIngredient1,
+                    element.strIngredient2,
+                    element.strIngredient3,
+                    element.strIngredient4,
+                    element.strIngredient5,
+                    element.strIngredient6,
+                    element.strIngredient7,
+                    element.strIngredient8,
+                    element.strIngredient9,
+                    element.strIngredient10,
+                    element.strIngredient11,
+                    element.strIngredient12,
+                    element.strIngredient13,
+                    element.strIngredient14,
+                    element.strIngredient15,
+                    element.strIngredient16,
+                    element.strIngredient17,
+                    element.strIngredient18,
+                    element.strIngredient19,
+                    element.strIngredient20],
+
+                    cantidades: [element.strMeasure1,
+                    element.strMeasure2,
+                    element.strMeasure3,
+                    element.strMeasure4,
+                    element.strMeasure5,
+                    element.strMeasure6,
+                    element.strMeasure7,
+                    element.strMeasure8,
+                    element.strMeasure9,
+                    element.strMeasure10,
+                    element.strMeasure11,
+                    element.strMeasure12,
+                    element.strMeasure13,
+                    element.strMeasure14,
+                    element.strMeasure15,
+                    element.strMeasure16,
+                    element.strMeasure17,
+                    element.strMeasure18,
+                    element.strMeasure19,
+                    element.strMeasure20,],
 
                     preparacion: element.strInstructions,
                     imagen: element.strMealThumb,
                     categoria: element.strCategory
-
-
-                    
-
-
                 }
-                
-                
-                createReceta(receta,tresRecetas);
 
-
-                
-            //});
+                createReceta(receta, tresRecetas);
             }
-            
         }
     });
-
 }
 
+function createReceta(receta, myDiv) {
 
-function createReceta(receta,myDiv){
-
-    const div1= document.createElement("div");
+    const div1 = document.createElement("div");
     div1.classList.add('card')
-    const div2= document.createElement("div");
+    const div2 = document.createElement("div");
     div1.classList.add('card-body')
 
-    const img1=document.createElement("img");
-    img1.src=receta.imagen;
+    const img1 = document.createElement("img");
+    img1.src = receta.imagen;
     img1.classList.add('img-fluid');
 
-    const h3= document.createElement('h3');
-    h3.innerText=receta.nombre;
+    const h3 = document.createElement('h3');
+    h3.innerText = receta.nombre;
 
-    const boton= document.createElement('button');
-    boton.innerText="Ver más";
+    const boton = document.createElement('button');
+    boton.innerText = "Ver más";
     boton.classList.add('btn');
     boton.classList.add('btn-primary');
-
-
-
 
     div2.appendChild(img1);
     div2.appendChild(h3);
@@ -139,27 +106,85 @@ function createReceta(receta,myDiv){
     div1.appendChild(div2);
 
     myDiv.appendChild(div1);
-
-
 }
 
-function busquedaRandom(){
+function busquedaRandom() {
+    // Obtenemos la sección donde se mostrarán los resultados
+    let carrusel = document.getElementById("carrusel");
 
+    // Lookup a single random meal
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php') // Para buscar una receta aleatoria
+        .then(response =>
+            response.json()
+        ).then(data => {
+            let resultados = "";
+            if (data.meals) { // Existe ese elemento? 
+                //console.log(data.meals);
+                resultados += `
+                        <div class="card card-body" id="${data.meals[0].idMeal}">
+                            <div>
+                                <img class="card-img img-fluid" src="${data.meals[0].strMealThumb}" alt="food">
+                                <h3>${data.meals[0].strMeal}</h3>
+                                <button class"btn btn-lg btn-primary" id="${data.meals[0].idMeal}">Ver más</button>
+                            </div>
+                        </div>
+                    `;
+            }
+            carrusel.innerHTML = resultados;
+        }).catch((error) => `failed.${error}`);;
 }
 
-function busquedaTexto(){
-    
+/**
+ * Función que realizara la busqueda de recetas leyendo el valor del input text
+ */
+function busquedaTexto()  {
+    // Obtendremos el valor del texto del input para buscar una receta por ingrediente principal
+    let searchInput = document.getElementById("search-input");
+    let ingrediente = searchInput.value.trim();
+    // Obtenemos la sección donde se mostrarán los resultados
+    let carrusel = document.getElementById("carrusel");
+
+    //Validamos que no se mande un string vacio para buscar
+    if (ingrediente === "") {
+        alert("Debe ingresar un ingrediente");
+    } else {
+        //fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a='+searchInput) // Nota: si se quiere buscar por por área descomentar esta linea
+        fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=' + ingrediente) // Para buscar por ingrediente principal
+            .then(response =>
+                response.json()
+            ).then(data => {
+                let resultados = "";
+                if (data.meals) {
+                    data.meals.forEach(element => {
+                        //console.log(element);
+                        resultados += `
+                            <div class="card card-body" id="${element.idMeal}">
+                                <div>
+                                    <img class="card-img img-fluid" src="${element.strMealThumb}" alt="food">
+                                    <h3>${element.strMeal}</h3>
+                                    <button class"btn btn-lg btn-primary" id="${element.idMeal}">Ver más</button>
+                                </div>
+                            </div>
+                        `;
+                    });
+                } else {
+                    alert("No se encontró ninguna receta con ese ingrediente.");
+                    searchInput.value = ''; // limpiamos el input text
+                }
+                carrusel.innerHTML = resultados;
+            }).catch((error) => `failed.${error}`);;
+    }
 }
-/*
-const getMeals = () => {
-  const meals = fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a=chinese')
-    .then((response) => response.json())
-    .then((result) => result)
-    .catch((error) => `failed.${error}`);
 
-  return meals;
-};
-
-const data = await getMeals();
-const theMeals = data.meals;
-console.log(data.meals);*/
+function creaModal(idReceta, event){
+    event.preventDefault();
+    console.log(idReceta);
+    if(idReceta !== undefined) {
+        // Lookup full meal details by id
+        fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + idReceta)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        }).catch(error => `failed.${error}`);
+    }
+}
