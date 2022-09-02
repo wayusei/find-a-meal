b09f9973a747f1557ebd6dee3e5d847e328d29ef
sergiomm84 <sergiomonterrubio@outlook.com>
2022-09-01 21:05:05 -0500

    index.html, connection.js Corregido el issue de la desaparición del modal al hacer clic

--- src/js/connection.js
+++ src/js/connection.js
@@ -7,113 +7,10 @@ let btnRandom = document.getElementById("btn_random");
 btnRandom.addEventListener('click', busquedaRandom);
 
 
-/*function indexLoad() {
-    //console.log("Loaded")
-
-    const tresRecetas = document.getElementById("carrusel");
-    const texto = "b";
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${texto}`;
-    fetch(url).then((res) => {
-        if (res.status != "200") {
-            //console.log(res);
-        }
-        else {
-            return res.json();
-        }
-    }).then((data) => {
-        if (data) {
-
-            //console.log(data.meals);
-            //data.meals.forEach(element => {
-            for (let i = 0; i < 3; i++) {
-                const element = data.meals[i];
-
-                const receta = {
-                    nombre: element.strMeal,
-                    ingredientes: [element.strIngredient1,
-                    element.strIngredient2,
-                    element.strIngredient3,
-                    element.strIngredient4,
-                    element.strIngredient5,
-                    element.strIngredient6,
-                    element.strIngredient7,
-                    element.strIngredient8,
-                    element.strIngredient9,
-                    element.strIngredient10,
-                    element.strIngredient11,
-                    element.strIngredient12,
-                    element.strIngredient13,
-                    element.strIngredient14,
-                    element.strIngredient15,
-                    element.strIngredient16,
-                    element.strIngredient17,
-                    element.strIngredient18,
-                    element.strIngredient19,
-                    element.strIngredient20],
-
-                    cantidades: [element.strMeasure1,
-                    element.strMeasure2,
-                    element.strMeasure3,
-                    element.strMeasure4,
-                    element.strMeasure5,
-                    element.strMeasure6,
-                    element.strMeasure7,
-                    element.strMeasure8,
-                    element.strMeasure9,
-                    element.strMeasure10,
-                    element.strMeasure11,
-                    element.strMeasure12,
-                    element.strMeasure13,
-                    element.strMeasure14,
-                    element.strMeasure15,
-                    element.strMeasure16,
-                    element.strMeasure17,
-                    element.strMeasure18,
-                    element.strMeasure19,
-                    element.strMeasure20,],
-
-                    preparacion: element.strInstructions,
-                    imagen: element.strMealThumb,
-                    categoria: element.strCategory
-                }
-
-                createReceta(receta, tresRecetas);
-            }
-        }
-    });
-}
-
-
-
-function createReceta(receta, myDiv) {
-
-    const div1 = document.createElement("div");
-    div1.classList.add('card')
-    const div2 = document.createElement("div");
-    div1.classList.add('card-body')
-
-    const img1 = document.createElement("img");
-    img1.src = receta.imagen;
-    img1.classList.add('img-fluid');
-
-    const h3 = document.createElement('h3');
-    h3.innerText = receta.nombre;
-
-    const boton = document.createElement('button');
-    boton.innerText = "Ver más";
-    boton.classList.add('btn');
-    boton.classList.add('btn-primary');
-
-    div2.appendChild(img1);
-    div2.appendChild(h3);
-    div2.appendChild(boton);
-    div1.appendChild(div2);
-
-    myDiv.appendChild(div1);
-}*/
 
 function createTemplate(data){
             const node1 = document.getElementById("carrusel");
+            const node2 = document.getElementById("verReceta");
             
              data.meals.forEach(element => {
                         
@@ -149,7 +46,20 @@ function createTemplate(data){
                                     </div>
                                 </div>    
 
-                                <div class="modal fade" id="modal${receta.id}" tabindex="-1" role="dialog" > 
+                              
+
+
+
+                        `;
+
+                        node.innerHTML = resultados;
+                        
+                        node.addEventListener ("click", createModal);
+                        node.myID=receta.id;
+                        node1.appendChild(node);
+
+
+                        let resultadoModal=`  <div class="modal fade" id="modal${receta.id}" tabindex="-1" role="dialog" > 
 
                                     <!------------------ ------>
 
@@ -182,17 +92,13 @@ function createTemplate(data){
                                                                          
 
 
-                                    </div> <!--- -->
+                                    </div> <!--- -->`;
+
+                        const nodeModal = document.createElement('div');
 
 
-
-                        `;
-
-                        node.innerHTML = resultados;
-                        
-                        node.addEventListener ("click", createModal);
-                        node.myID=receta.id;
-                        node1.appendChild(node);
+                        nodeModal.innerHTML= resultadoModal;
+                        node2.appendChild(nodeModal);
                         
                          
 
